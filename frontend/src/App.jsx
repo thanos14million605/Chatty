c837702d28a7a7be1525823c7a0020b6d0f17a28
Ebrima Gajaga <gajaga.dev@gmail.com>
2026-03-25 10:09:44 +0530

    Modified App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
